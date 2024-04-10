8c7ee0996b9458ffc9b17f4c504ef21d2b455be6
TheCPP <buch.toni@outlook.com>
2024-04-10 17:01:18 +0200

    disasm

--- src/disasm.rs
+++ src/disasm.rs
@@ -1,7 +1,39 @@
 use std::error::Error;
+use pad::PadStr;
 
 use object::{File, Object, ObjectSection, ObjectSymbol, Symbol, SymbolKind};
 use PrintLib::colorize::Colorize;
+
+use capstone::prelude::*;
+
+fn disasm(cs: &Capstone, machine_code: &[u8]) -> Result<Vec<(String, String)>, Box<dyn Error>> {
+    let disasm = cs.disasm_all(machine_code, 0)?;
+    let mut ret = vec![];
+
+    for instruction in disasm.iter() {
+
+        let fmt_arry = {
+            let bytes = instruction.bytes();
+            let mut fmt = String::new();
+
+            for b in bytes {
+                fmt.push_str(&format!("{:#02x} ", b));
+            }
+
+            fmt
+        };
+        let instruction = format!("{}", instruction);
+        let fmt_instruction = instruction.split(':').collect::<Vec<&str>>()[1];
+
+        ret.push(
+            (fmt_arry, fmt_instruction.to_string())
+        )
+    }
+
+    Ok(ret)
+}
+
+    
 
 pub fn print(file: &File) -> Result<(), Box<dyn Error>> {
     let syms: Vec<Symbol> = {
@@ -15,6 +47,14 @@ pub fn print(file: &File) -> Result<(), Box<dyn Error>> {
 
         ret
     };
+
+    let cs = Capstone::new()
+        .x86()
+        .mode(arch::x86::ArchMode::Mode64)
+        .syntax(arch::x86::ArchSyntax::Att)
+        .detail(true)
+        .build()
+        .expect("Failed to create Capstone object");
 
     for sym in syms {
         let name = sym.name()?;
@@ -31,20 +71,19 @@ pub fn print(file: &File) -> Result<(), Box<dyn Error>> {
 
         let data = section.data()?;
 
-        let mut row_bytes = 0;
+        let instrs = disasm(&cs, data)?;
 
-        for byte in data {
-            if row_bytes == 5 {
-                row_bytes = 0;
-                println!()
+        let mut longest = 0;
+        for i in &instrs {
+            if i.0.len() > longest {
+                longest = i.0.len();
             }
-            if row_bytes == 0 {
-                print!("   ");
-            }
-            row_bytes += 1;
-
-            print!("{:#04X} ", byte);
         }
+
+        for instr in instrs {
+            println!("   {} {} {}", instr.0.pad_to_width(longest), "|".gray().bold(), instr.1.green());
+        }
+
 
         println!();
     }
